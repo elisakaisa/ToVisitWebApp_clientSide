5b7b29b6a9d1e6bacde4f509286bcaeedfbb2343
elisakaisa <elisa.k.perini@gmail.com>
2022-08-21 18:24:41 +0200

    sorting works in both directions

--- src/components/VisitTable.js
+++ src/components/VisitTable.js
@@ -1,28 +1,78 @@
-import React, { useState } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 
 import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-import Link from '@mui/material/Link'
-import { MoreHoriz } from '@mui/icons-material'
-import { TableSortLabel } from '@mui/material'
+import { TableHead, TableSortLabel } from '@mui/material'
 
-import SimpleTableHead from './table/SimpleTableHead'
 import SimpleTableBody from './table/SimpleTableBody'
 
 
 const VisitTable = ({ visits }) => {
 
+    // states
+    const [sortedVisits, setSortedVisits] = useState(visits)
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('what')
+
+    // ma  kes sure the state is actually saved, otherwise empty array
+    useEffect(() => {
+        setSortedVisits(visits)
+    },[visits]) 
+
+    // TODO: figure out a nicer way
+    const onSortWhat = () => {
+        if (order === 'asc') {
+            setSortedVisits([...visits].sort((a, b) => a.what.localeCompare(b.what)))
+        } else {
+            setSortedVisits([...visits].sort((a, b) => b.what.localeCompare(a.what)))
+        }
+        const isAsc = orderBy === 'what' && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc')
+        setOrderBy('what')
+    }
+    const onSortWhere = () => {
+        if (order === 'asc') {
+            setSortedVisits([...visits].sort((a, b) => a.where.localeCompare(b.where)))
+        } else {
+            setSortedVisits([...visits].sort((a, b) => b.where.localeCompare(a.where)))
+        }
+        const isAsc = orderBy === 'where' && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc')
+        setOrderBy('where')
+    }
+
     return (
         <Paper elevation={3} sx= {{ p:1, backgroundColor: '#040a1a' }}>
                 <TableContainer components={Paper}>
                     <Table sx={{ minWidth: 650}} aria-label="simple table">
-                        <SimpleTableHead />
-                        <SimpleTableBody visits={visits}/>
+                    <TableHead>
+                            <TableRow>
+                            <TableCell sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>
+                                <TableSortLabel 
+                                    onClick={onSortWhat}
+                                    active={orderBy === 'what'}
+                                    direction={orderBy === 'what' ? order : 'asc'}>
+                                        What
+                                </TableSortLabel>
+                            </TableCell>
+                            <TableCell align="left" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>
+                                <TableSortLabel 
+                                    onClick={onSortWhere}
+                                    active={orderBy === 'where'}
+                                    active={orderBy === 'where'}
+                                    direction={orderBy === 'where' ? order : 'asc'}>
+                                        Where
+                                </TableSortLabel>
+                            </TableCell>
+                            <TableCell align="left" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>Category</TableCell>
+                            <TableCell align="center" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>Price</TableCell>
+                            <TableCell align="center" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>Ease of organization</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <SimpleTableBody visits={sortedVisits}/>
                     </Table>
                 </TableContainer>
             </Paper>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,69 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { Link as RouterLink } from 'react-router-dom'
 
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
-import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
-import { MoreHoriz } from '@mui/icons-material'
-import SimpleTableHead from '../components/table/SimpleTableHead'
-import SimpleTableBody from '../components/table/SimpleTableBody'
-import { TableSortLabel } from '@mui/material'
+import VisitTable from '../components/VisitTable'
 
 
 const Home = () => {
 
     let visits = useSelector(state => state.visits)
 
-    const [sortedVisits, setSortedVisits] = useState(visits)
-
-    // makes sure the state is actually saved, otherwise empty array
-    useEffect(() => {
-        setSortedVisits(visits)
-    },[visits]) 
-
-    // TODO: figure out a nicer way
-    const onSortWhat = () => {
-        setSortedVisits([...visits].sort((a, b) => a.what.localeCompare(b.what)))
-    }
-    const onSortWhere = () => {
-        setSortedVisits([...visits].sort((a, b) => a.where.localeCompare(b.where)))
-    }
-
+    // make a copy of visits to avoid state mutation errors
+    let sortedVisits = [...visits].sort((a, b) => a.what.localeCompare(b.what))
 
     return (
         <>
             <Typography variant="h5" component="div" sx= {{ p:1, m:1 }} gutterBottom>
                 Places to visit in the Stockholm area
             </Typography>
-            <TableContainer components={Paper}>
-                    <Table sx={{ minWidth: 650}} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                            <TableCell sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>
-                                <TableSortLabel onClick={onSortWhat}>
-                                    What
-                                </TableSortLabel>
-                            </TableCell>
-                            <TableCell align="left" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>
-                                <TableSortLabel onClick={onSortWhere}>
-                                    Where
-                                </TableSortLabel>
-                            </TableCell>
-                            <TableCell align="left" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>Category</TableCell>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>Price</TableCell>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', fontSize: 'h7.fontSize'}}>Ease of organization</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <SimpleTableBody visits={sortedVisits}/>
-                    </Table>
-            </TableContainer>
+            <VisitTable visits={sortedVisits} />
         </>  
     )
 }
